ab78759850358720f1a6bc092aa098793182bd41
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-08-23 17:08:11 +0530

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -36,7 +36,7 @@ st.subheader("Enter the car details:")
 year = st.number_input("Year", min_value=1990, max_value=2023, value=2020, step=1)
 mileage = st.number_input("Mileage (in km)", min_value=0, value=50000, step=1000)
 engine = st.number_input("Engine (in cc)", min_value=800, max_value=6000, value=1500, step=100)
-transmission = st.selectbox("Transmission", ["Manual", "Automatic"])
+#transmission = st.selectbox("Transmission", ["Manual", "Automatic"])
 owner = st.selectbox("Previous Owners", ["First", "Second", "Third", "Fourth & Above"])
 fuel = st.selectbox("Fuel Type", ["Petrol", "Diesel", "CNG"])
 
@@ -45,7 +45,7 @@ new_data = pd.DataFrame({
     'Year': [year],
     'Mileage': [mileage],
     'Engine': [engine],
-    'Transmission': [transmission],
+    #'Transmission': [transmission],
     'Owner': [owner],
     'Fuel_Type': [fuel]
 })
